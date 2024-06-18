cbb007e4e69ba6fb0340e5094835c15b1459c9e3
Francisco Vozzi <franciscovozzi@outlook.com>
2024-06-18 09:52:51 -0300

    Day 2 challenge added.

---
+++ projectData.py
@@ -0,0 +1,17 @@
+#datos del proyecto
+
+projectName = "180 Days of Data Science";
+startDate = "June 17 of 2024";
+responsible = "Francisco Vozzi"
+description = "A challenge to enhance my Data Science skills through atomic challenges."
+
+print("Project name: ",projectName);
+print("Start date: ", startDate);
+print("Responsible: ", responsible);
+print("Description of the project:", description);
+
+teamMembers = ["Francisco"]
+budget = 0;
+
+print("Team Members:",teamMembers)
+print("The budget is:", budget)
